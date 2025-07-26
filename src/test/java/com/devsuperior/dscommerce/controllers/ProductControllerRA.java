8ae91b9efeec56725e098c1a2ccf7e2f8a798f06
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-26 14:13:36 -0300

    Teste filtra busca de produtos paginados

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -10,11 +10,14 @@ import static org.hamcrest.Matchers.*;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void setUp(){
 
         baseURI = "http://localhost:8080";
+
+        productName = "Macbook";
     }
 
     @Test
@@ -36,5 +39,15 @@ public class ProductControllerRA {
 
         given().get("/products?page=0").then().statusCode(200)
                 .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty(){
+
+        given().get("/products?name={productName}", productName).then().statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"))
+                .body("content.price[0]", is(1250.0F))
+                .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
     }
 }
