bf84df13c053af7706b07656184b01eaf4971e78
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-07-26 14:22:18 -0300

    Teste filtra busca de produtos preço maior que 2000

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -50,4 +50,11 @@ public class ProductControllerRA {
                 .body("content.price[0]", is(1250.0F))
                 .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
     }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000(){
+
+        given().get("/products?size=25").then().statusCode(200)
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
+    }
 }
