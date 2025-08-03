4a5245884279b8375eba82f2d8702399d0d2b995
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-08-03 08:07:01 -0300

    Teste CategoryControllerRA

--- src/test/java/com/devsuperior/dscommerce/tests/TokenUtil.java
+++ src/test/java/com/devsuperior/dscommerce/tests/TokenUtil.java
@@ -4,8 +4,6 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
 
 public class TokenUtil {
 
